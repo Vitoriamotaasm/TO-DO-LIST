e35371db2cbe9a93baeed5ed053c9a7f5d0481f2
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-02-10 21:25:38 -0300

    projeto finalizado

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true, 
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  tasks: { name: string, completed: boolean }[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ name: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  toggleTaskCompletion(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
